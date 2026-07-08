6baf4a0d3f23488a714f11d26b6a55cf46a42cdd
Brbodd <b.z.10231384@gmail.com>
2026-07-08 19:55:11 +0330

    feat: add project routing and initialize templates , make CSS files

--- splus_project/accounts/views.py
+++ splus_project/accounts/views.py
@@ -1,2 +1,13 @@
 from django.shortcuts import render
 
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    return render(request, 'accounts/logout.html')
+
+def profile(request):
+    return render(request, 'accounts/profile.html')
+
+def register(request):
+    return render(request, 'accounts/register.html')
