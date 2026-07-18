446a2157bcc67e8ad07841167894bb9205ddd836
Brbodd <b.z.10231384@gmail.com>
2026-07-18 19:41:46 +0330

    Move navigation include to base template

--- splus_project/products/views.py
+++ splus_project/products/views.py
@@ -3,32 +3,39 @@ from .models import Product, Brand
 
 def product_list(request):
 
-    products = Product.objects.all()
-    # products = range(9)
+    # products = Product.objects.all()
+    products = range(9)
 
-    brand = request.GET.getlist("brand")
-    min_price = request.GET.get("min_price")
-    max_price = request.GET.get("max_price")
-    in_stock = request.GET.get("in_stock")
+    # products_list = [
+    #     {"img": "https://www.seikopluswatch.com/wp-content/uploads/2024/08/seikoplus.png",
+    #      "name": "سیکو پلاس مردانه",
+    #      "code": "PH-1051",
+    #      "price": "۱۱.۲۹۰.۰۰۰"},
+    # ]
 
-    if brand:
-        products = products.filter(brand__slug__in=brand)
+    # type = request.GET.getlist("type")
+    # min_price = request.GET.get("min_price")
+    # max_price = request.GET.get("max_price")
+    # in_stock = request.GET.get("in_stock")
 
-    if min_price:
-        products = products.filter(price__gte=min_price)
+    # if brand:
+    #     products = products.filter(brand__slug__in=brand)
 
-    if max_price:
-        products = products.filter(price__lte=max_price)
+    # if min_price:
+    #     products = products.filter(price__gte=min_price)
 
-    if in_stock:
-        products = products.filter(quantity__gt=0)
+    # if max_price:
+    #     products = products.filter(price__lte=max_price)
 
-    sort = request.GET.get("sort")
-    if sort == "cheap":
-        products = products.order_by("price")
+    # if in_stock:
+    #     products = products.filter(quantity__gt=0)
 
-    elif sort == "expensive":
-        products = products.order_by("-price")
+    # sort = request.GET.get("sort")
+    # if sort == "cheap":
+    #     products = products.order_by("price")
+
+    # elif sort == "expensive":
+    #     products = products.order_by("-price")
 
     # elif sort == "newest":
     #     products = products.order_by("-created_at")
@@ -43,22 +50,7 @@ def product_list(request):
 
 
 def product_detail(request, slug):
-    # products = [
-    #     {
-    #         "name": "Casio A158",
-    #         "code": "PH-1051"
-    #     },
-    #     {
-    #         "name": "Seiko 5",
-    #         "code": "PH-1052"
-    #     },
-    #     {
-    #         "name": "Citizen Eco Drive",
-    #         "code": "PH-1053"
-    #     }
-    # ]
-    products = range(9)
-    return render(request, "products/product_detail.html", { 'products': products })
+    return render(request, "products/product_detail.html")
 
 def category_products(request, slug):
     pass
