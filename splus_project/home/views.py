01260d7d25cc714b4c3ebc526f4340ead1c1d145
Brbodd <b.z.10231384@gmail.com>
2026-07-18 20:25:46 +0330

    Pass product data to reusable product card template

--- splus_project/home/views.py
+++ splus_project/home/views.py
@@ -1,7 +1,39 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'home/home.html')
+        
+    products = [
+        {"img": "https://www.seikopluswatch.com/wp-content/uploads/2024/08/seikoplus.png",
+         "name": "سیکو پلاس مردانه",
+         "code": "PH-1051",
+         "price": "۱۱.۲۹۰.۰۰۰",
+         "quantity": 0},
+        {"img": "https://www.seikopluswatch.com/wp-content/uploads/2024/08/seikoplus.png",
+         "name": "سیکو پلاس زنانه",
+         "code": "PH-1052",
+         "price": "۱۲.۶۹۰.۰۰۰",
+         "quantity": 3},
+        {"img": "https://www.seikopluswatch.com/wp-content/uploads/2024/08/seikoplus.png",
+         "name": "کاسیو پلاس مردانه",
+         "code": "PH-1053",
+         "price": "۱۰.۱۹۰.۰۰۰",
+         "quantity": 10},
+        {"img": "https://www.seikopluswatch.com/wp-content/uploads/2024/08/seikoplus.png",
+         "name": "سیکو پلاس مردانه",
+         "code": "PH-1054",
+         "price": "۱۵.۰۰۰.۰۰۰",
+         "quantity": 1},
+        {"img": "https://www.seikopluswatch.com/wp-content/uploads/2024/08/seikoplus.png",
+         "name": "سیکو پلاس مردانه و زنانه",
+         "code": "PH-1055",
+         "price": "۹.۸۰۷.۰۰۰",
+         "quantity": 0},
+    ]
+    
+    return render(request, 'home/home.html',
+        {
+            "products": products
+        })
 
 def about(request):
     return render(request, 'home/about.html')
--- splus_project/products/views.py
+++ splus_project/products/views.py
@@ -4,14 +4,33 @@ from .models import Product, Brand
 def product_list(request):
 
     # products = Product.objects.all()
-    products = range(9)
 
-    # products_list = [
-    #     {"img": "https://www.seikopluswatch.com/wp-content/uploads/2024/08/seikoplus.png",
-    #      "name": "سیکو پلاس مردانه",
-    #      "code": "PH-1051",
-    #      "price": "۱۱.۲۹۰.۰۰۰"},
-    # ]
+    products = [
+        {"img": "https://www.seikopluswatch.com/wp-content/uploads/2024/08/seikoplus.png",
+         "name": "سیکو پلاس مردانه",
+         "code": "PH-1051",
+         "price": "۱۱.۲۹۰.۰۰۰"},
+        {"img": "https://www.seikopluswatch.com/wp-content/uploads/2024/08/seikoplus.png",
+         "name": "سیکو پلاس مردانه",
+         "code": "PH-1052",
+         "price": "۱۲.۶۹۰.۰۰۰"},
+        {"img": "https://www.seikopluswatch.com/wp-content/uploads/2024/08/seikoplus.png",
+         "name": "سیکو پلاس مردانه",
+         "code": "PH-1053",
+         "price": "۱۰.۱۹۰.۰۰۰"},
+        {"img": "https://www.seikopluswatch.com/wp-content/uploads/2024/08/seikoplus.png",
+         "name": "سیکو پلاس مردانه",
+         "code": "PH-1054",
+         "price": "۱۵.۰۰۰.۰۰۰"},
+        {"img": "https://www.seikopluswatch.com/wp-content/uploads/2024/08/seikoplus.png",
+         "name": "سیکو پلاس زنانه",
+         "code": "PH-1055",
+         "price": "۱۷.۴۹۰.۰۰۰"},
+        {"img": "https://www.seikopluswatch.com/wp-content/uploads/2024/08/seikoplus.png",
+         "name": " ست سیکو پلاس مردانه",
+         "code": "PH-1056",
+         "price": "۱۳.۸۹۰.۰۰۰"},
+    ]
 
     # type = request.GET.getlist("type")
     # min_price = request.GET.get("min_price")
