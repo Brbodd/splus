95a31e86e113a86a118dc9b74847063bb4dc3f85
Brbodd <b.z.10231384@gmail.com>
2026-07-18 18:35:13 +0330

    Implement product listing layout and filtering UI

--- splus_project/products/models.py
+++ splus_project/products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    brand = models.ForeignKey(
+        Brand,
+        on_delete=models.CASCADE,
+        related_name="products"
+    )
+
+    price = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- splus_project/products/views.py
+++ splus_project/products/views.py
@@ -1,21 +1,64 @@
 from django.shortcuts import render
+from .models import Product, Brand
 
 def product_list(request):
-    # products = Product.objects.all()
 
-    # context = {
-    #     "products": products,
-    # }
-    return render(request, "products/product_list.html")
+    products = Product.objects.all()
+    # products = range(9)
+
+    brand = request.GET.getlist("brand")
+    min_price = request.GET.get("min_price")
+    max_price = request.GET.get("max_price")
+    in_stock = request.GET.get("in_stock")
+
+    if brand:
+        products = products.filter(brand__slug__in=brand)
+
+    if min_price:
+        products = products.filter(price__gte=min_price)
+
+    if max_price:
+        products = products.filter(price__lte=max_price)
+
+    if in_stock:
+        products = products.filter(quantity__gt=0)
+
+    sort = request.GET.get("sort")
+    if sort == "cheap":
+        products = products.order_by("price")
+
+    elif sort == "expensive":
+        products = products.order_by("-price")
+
+    # elif sort == "newest":
+    #     products = products.order_by("-created_at")
+
+    return render(
+        request,
+        "products/product_list.html",
+        {
+            "products": products
+        }
+    )
 
 
 def product_detail(request, slug):
-    # product = get_object_or_404(Product, slug=slug)
-
-    # context = {
-    #     "product": product,
-    # }
-    return render(request, "products/product_detail.html")
+    # products = [
+    #     {
+    #         "name": "Casio A158",
+    #         "code": "PH-1051"
+    #     },
+    #     {
+    #         "name": "Seiko 5",
+    #         "code": "PH-1052"
+    #     },
+    #     {
+    #         "name": "Citizen Eco Drive",
+    #         "code": "PH-1053"
+    #     }
+    # ]
+    products = range(9)
+    return render(request, "products/product_detail.html", { 'products': products })
 
 def category_products(request, slug):
     pass
