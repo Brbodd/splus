4a24bb8c4b6f4e4e6a8e90b3cd19d1cad034928f
Brbodd <b.z.10231384@gmail.com>
2026-07-29 15:41:14 +0330

    use correct slug for product-detail url, write some functions in models.py, use them in other files

--- splus_project/products/models.py
+++ splus_project/products/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.text import slugify
+from django.urls import reverse
 
 class Brand(models.Model):
     name = models.CharField(max_length=100)
@@ -6,19 +8,27 @@ class Brand(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(default='', null=False, unique=True)
     code = models.CharField(max_length=50)
 
-    brand = models.ForeignKey(
-        Brand,
-        on_delete=models.CASCADE,
-        related_name="products"
-    )
+    # brand = models.ForeignKey(
+    #     Brand,
+    #     on_delete=models.CASCADE,
+    #     related_name="products"
+    # )
 
     price = models.PositiveIntegerField()
-    quantity = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(default='', null=False)
 
-    created_at = models.DateTimeField(auto_now_add=True)
+    image_src = models.CharField(default='', max_length=350)
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("product-detail", args=[self.slug])
+    
     def __str__(self):
         return self.name
--- splus_project/products/views.py
+++ splus_project/products/views.py
@@ -1,61 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Product, Brand
 
 def product_list(request):
 
-    # products = Product.objects.all()
-
-    products = [
-        {"img": "https://www.seikopluswatch.com/wp-content/uploads/2024/08/seikoplus.png",
-         "name": "سیکو پلاس مردانه",
-         "code": "PH-1051",
-         "price": "۱۱.۲۹۰.۰۰۰",
-         "quantity": 0,
-         "slug": "seiko__ph_1051"},
-        {"img": "https://www.seikopluswatch.com/wp-content/uploads/2024/08/seikoplus.png",
-         "name": "سیکو پلاس زنانه",
-         "code": "PH-1052",
-         "price": "۱۲.۶۹۰.۰۰۰",
-         "quantity": 3,
-         "slug": "seiko__ph_1052"},
-        {"img": "https://www.seikopluswatch.com/wp-content/uploads/2024/08/seikoplus.png",
-         "name": "کاسیو پلاس مردانه",
-         "code": "PH-1053",
-         "price": "۱۰.۱۹۰.۰۰۰",
-         "quantity": 10,
-         "slug": "casio__ph_1053"},
-        {"img": "https://www.seikopluswatch.com/wp-content/uploads/2024/08/seikoplus.png",
-         "name": "سیکو پلاس مردانه",
-         "code": "PH-1054",
-         "price": "۱۵.۰۰۰.۰۰۰",
-         "quantity": 1,
-         "slug": "seiko__ph_1054"},
-        {"img": "https://www.seikopluswatch.com/wp-content/uploads/2024/08/seikoplus.png",
-         "name": "سیکو پلاس مردانه و زنانه",
-         "code": "PH-1055",
-         "price": "۹.۸۰۷.۰۰۰",
-         "quantity": 0,
-         "slug": "seiko__ph_1055"},
-        {"img": "https://www.seikopluswatch.com/wp-content/uploads/2024/08/seikoplus.png",
-         "name": "سیکو پلاس زنانه",
-         "code": "PH-1056",
-         "price": "۱۰.۸۰۷.۰۰۰",
-         "quantity": 3,
-         "slug": "seiko__ph_1056"},        
-         {"img": "https://www.seikopluswatch.com/wp-content/uploads/2024/08/seikoplus.png",
-         "name": "سیکو مردانه",
-         "code": "PH-1058",
-         "price": "۱۱.۰۰۰.۰۰۰",
-         "quantity": 7,
-         "slug": "seiko__ph_1058"},        
-         {"img": "https://www.seikopluswatch.com/wp-content/uploads/2024/08/seikoplus.png",
-         "name": "سیکو پلاس مردانه و زنانه",
-         "code": "PH-1059",
-         "price": "۱۲.۲۳۰.۰۰۰",
-         "quantity": 5,
-         "slug": "seiko__ph_1059"},         
-    ]
-
+    products = Product.objects.all()
     # type = request.GET.getlist("type")
     # min_price = request.GET.get("min_price")
     # max_price = request.GET.get("max_price")
@@ -94,13 +42,6 @@ def product_list(request):
 
 def product_detail(request, slug):
 
-    '''
-    just write this lines after run DB
-    from django.shortcuts import get_object_or_404
-
-    product = get_object_or_404(Product, slug=slug)
-    '''
-
     products = [
         {"img": "https://www.seikopluswatch.com/wp-content/uploads/2024/08/seikoplus.png",
          "name": "سیکو پلاس مردانه",
@@ -151,13 +92,8 @@ def product_detail(request, slug):
          "quantity": 5,
          "slug": "seiko__ph_1059"},         
     ]
-
-    product = None
-
-    for item in products:
-        if item["slug"] == slug:
-            product = item
-            break
+    
+    product = get_object_or_404(Product, slug=slug)
 
     return render(
         request,
