30cb32708b80f588fb06409a7a6bc454321e8b6e
Brbodd <b.z.10231384@gmail.com>
2026-08-24 14:01:44 +0330

    Improve Repository document and Project structure

--- splus_project/products/views.py
+++ splus_project/products/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from .models import Product, ProductCategory
+from .models import Product
 
 class ProductListView(ListView):
     template_name = 'products/product_list.html'
